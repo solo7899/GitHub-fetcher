f0473bc72bad441d17cdb3ddc3901fd5f3924431
hamidreza <dolati.hamidreza78@gmail.com>
2025-07-08 19:15:40 +0330

    changed a abit of code style, added output functionality

--- git_fetcher.py
+++ git_fetcher.py
@@ -6,29 +6,31 @@ import requests
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Fetch and process Git repositories.")
     parser.add_argument(
-        "--repo",
+        "--owner",
         type=str,
         required=True,
-        help="The URL of the Git repository to fetch.",
+        help="The owner of the Git acount to fetch.",
     )
     parser.add_argument(
         "--output",
-        type=str,
-        default="./output",
-        help="The directory where the fetched repository will be stored (default: ./output).",
+        action="store_true",
+        help="If set, output the fetched repository content to a file.",
     )
     return parser.parse_args()
 
+def output_json(api_output, filename):
+    with open(f"{filename}.json", "w") as file:
+        file.write(api_output)
 
 def connect_to_database():   
     conn = sqlite3.connect('repositories.db')
     cursor = conn.cursor()
     return conn, cursor
 
-
-def request_repository(repo_url):
+def request_repository(username):
+    url = f"https://api.github.com/users/{username}/repos"
     try:
-        response = requests.get(repo_url)
+        response = requests.get(url)
         response.raise_for_status()
         return response.text
     except requests.RequestException as e:
@@ -37,13 +39,18 @@ def request_repository(repo_url):
 
 if __name__ == "__main__":
     args = parse_arguments()
-    if not args.repo:
-        print("Error: --repo argument is required.")
+    if not args.owner:
+        print("Error: --owner argument is required.")
         exit(1)
-    print(f"Repository URL: {args.repo}")
+    print(f"Git owner: {args.owner}")
 
     conn, cursor = connect_to_database()
 
-    repo_content = request_repository(args.repo)
-    print(f"Fetched content from {args.repo}")
-    print(repo_content)
+    repo_content = request_repository(args.owner)
+    if repo_content is None:
+        print("Failed to fetch repository content.")
+        exit(1)
+    print(f"Fetched content from {args.owner}")
+
+    if args.output:
+        output_json(repo_content, args.owner)
